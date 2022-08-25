24d6bdf1fed09d9675784ea79ea5d59329305db5
juaness00 <juanessanchez01@gmail.com>
2022-08-24 23:04:12 -0400

    models.py

--- Config/models.py
+++ Config/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=255,unique= True)
     password = models.CharField(max_length=255)
-    userType = models.ForeignKey('UserType', on_delete=models.CASCADE,blank=True,null=True)
+    userType = models.ForeignKey('UserType', on_delete=models.CASCADE)
     def __str__(self):
         return self.username
     def __int__(self):
